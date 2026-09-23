8cacd7e9e1a313256e7b29f587b71235ee9b9476
mirza-z <mirza.zaimovic@edu.fit.ba>
2026-09-24 01:51:39 +0200

    update Angular build and deployment configuration

---
+++ kanban-board-client/src/environments/environments.production.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://kanban-api-ghrd.onrender.com/api',
+  googleClientId: '1060755725022-2vacfkph4q2kdvcs6hmjnr4ian9gkkqq.apps.googleusercontent.com',
+  demoBoardId: 'a1b2c3d4-0000-4000-8000-000000000001'
+};
